c99e5c01b1c744532356d36f99593736127e6095
KateLu666 <KateLuDev@outlook.com>
2023-11-23 15:19:23 -0500

    Finish

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">PLAY</button>
+  <ReactionBlock v-if="isPlaying" :delay="delay" @end="endGame" />
+  <ReactionResults v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import ReactionBlock from "./components/ReactionBlock.vue";
+import ReactionResults from "./components/ReactionResults.vue";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { ReactionBlock, ReactionResults },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
   },
 };
 </script>
@@ -22,5 +42,28 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button {
+  padding: 8px 30px;
+  background-color: white;
+  color: #4b7b8b;
+  border: 1px solid #4b7b8b;
+  border-radius: 20px;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  transition: background-color 0.3s, box-shadow 0.3s;
+}
+
+button:hover {
+  color: black;
+}
+
+button:disabled,
+button:disabled:hover {
+  color: #4b7b8b;
+  cursor: not-allowed;
+  background-color: white;
 }
 </style>
---
+++ src/components/ReactionResults.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="results">
+    <h3>Your reaction time is {{ score }}ms</h3>
+    <h2 class="rank">Your rank is {{ rank }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 100) {
+      this.rank = "Godlike";
+    } else if (this.score < 200) {
+      this.rank = "Pro";
+    } else if (this.score < 300) {
+      this.rank = "Average";
+    } else if (this.score < 400) {
+      this.rank = "Beginner";
+    } else {
+      this.rank = "Newcomer";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  color: #4b7b8b;
+  font-weight: bold;
+}
+</style>
